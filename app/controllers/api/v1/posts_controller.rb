262de4ae342f0c8de913e124311c31a8aa6c0df0
Khaled <Khaled195341@feng.bu.edu.eg>
2024-09-09 14:26:11 +0300

    posts controllers

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,8 +1,13 @@
-class PostsController < ApplicationController
+class Api::V1::PostsController < ApplicationController
+  before_action :set_post, only: [ :show ]
+
   def index
+    @posts = Post.all
+    render json: { success: true, posts: @posts }
   end
 
   def show
+    render json: { success: true, posts: @post}
   end
 
   def new
@@ -18,5 +23,15 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def post_params
+    params.permit(:title, :body, :tags)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
